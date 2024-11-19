d3f89cf2c9bb0135580301ab8ff673f82ba5e60e
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2024-11-19 21:53:07 +0100

    Add banking dashboard component

--- libs/banking/dashboard/feature/src/banking-dashboard/banking-dashboard.component.ts
+++ libs/banking/dashboard/feature/src/banking-dashboard/banking-dashboard.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {
+  OverviewUiComponent,
+  LatestTransactionsUiComponent,
+} from '@banking/dashboard/ui';
+import { Observable } from 'rxjs';
+import { DashboardCard, SharedDashboardComponent } from '@shared/ui';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'banking-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SharedDashboardComponent,
+    MatIconModule,
+    MatToolbarModule,
+  ],
+  selector: 'banking-dashboard',
   templateUrl: './banking-dashboard.component.html',
 })
-export class BankingDashboardComponent {}
+export class BankingDashboardComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards: Observable<DashboardCard[]>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.cards = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(({ matches }) => {
+        return [
+          {
+            title: 'Overview',
+            component: OverviewUiComponent,
+            data: {},
+            cols: 2,
+            rows: 0.5,
+          },
+          {
+            title: 'Latest transactions - Payment',
+            component: LatestTransactionsUiComponent,
+            data: { account: 'payment' },
+            cols: matches ? 2 : 1,
+            rows: 1,
+          },
+          {
+            title: 'Latest transactions - Savings',
+            component: LatestTransactionsUiComponent,
+            data: { account: 'savings' },
+            cols: matches ? 2 : 1,
+            rows: 1,
+          },
+        ];
+      })
+    );
+  }
+}
